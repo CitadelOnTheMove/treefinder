c8ade33ad0d9465b649afddeca71a10ba3f50bb3
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-06-10 22:16:54 +0200

    Made config almost automatic

--- Config.php
+++ Config.php
@@ -8,9 +8,12 @@
 /***********************************************************
  * Don't forget to insert the web root directory 
  * for example: "C:/wamp/www/" is the default root web dir in a standard wampServer setup)
+ * On most Linux servers, it should be: /var/www/
  ***********************************************************
  */
-define("HTDOCS_ROOT", "/code/");  
+//define("HTDOCS_ROOT", "/code/");  
+// Following should work in all situations (with trailing slash)
+define("HTDOCS_ROOT", dirname(dirname(__FILE__)) . '/');
 
 /*********************************************************** 
  * Replace 'localhost' with your IP address, 
@@ -18,22 +21,31 @@ define("HTDOCS_ROOT", "/code/");
  * connected to the same network 
  ***********************************************************
  */
-define("SERVERNAME", "http://localhost/"); 
+//define("SERVERNAME", "http://localhost/"); 
+// Automatically set server name - should work in most cases, use above setting if not
+$relative_path = $_SERVER['SERVER_NAME'] . dirname(dirname($_SERVER['PHP_SELF'])) . '/';
+if ($_SERVER['HTTPS']) {
+	define("SERVERNAME", "https://" . $relative_path);
+} else {
+	define("SERVERNAME", "http://" . $relative_path);
+}
 
 /*********************************************************** 
  * Leave all the settings below unchanged if you only want
  * to run the template with the default dataset and parameters.
  ***********************************************************
  */
- 
+
+// Base dir should be changed if you renamed the folder containing the app
 define("BASE_DIR", "treefinder/" );
 define("CLASSES_DIR", "php/");
 define("CLASSES", HTDOCS_ROOT . BASE_DIR . CLASSES_DIR);
 
 define("DEBUG", true);
 
-// dataset
+// dataset - see data folder for various example data files
 define("DATASET_FILE", HTDOCS_ROOT . BASE_DIR ."data/POI_trees_gent.json");
+//define("DATASET_FILE", HTDOCS_ROOT . BASE_DIR ."data/POI_trees_issy.json");
 define("DATASET_ID", 36);
 define("DATASET_URL", SERVERNAME . BASE_DIR . "dataset.php");
 define("USE_DATABASE", false);
@@ -50,4 +62,4 @@ define("DB_HOSTNAME", "127.0.0.1");
 define("DB_PORT", "3306");
 define("DB_NAME", "citadel");
 
-?>
+
